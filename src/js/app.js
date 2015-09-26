7f4b88e1329fbf565b3418e486738594bbc4ef78
Marc Easen <marc@easen.co.uk>
2015-09-26 18:25:44 +0100

    Initial version of the Pebble Control app

--- src/js/app.js
+++ src/js/app.js
@@ -6,83 +6,70 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var ajax = require('ajax');
+var _ = require('underscore');
+var traverson = require('traverson');
+var JsonHalAdapter = require('traverson-hal');
 
 var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.',
-  subtitleColor: 'indigo', // Named colors
-  bodyColor: '#9a0036' // Hex colors
+    title: 'Pebble Connect',
+    icon: 'images/menu_icon.png',
+    subtitle: 'Loading...'
 });
-
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }, {
-        title: 'Third Item',
-      }, {
-        title: 'Fourth Item',
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    backgroundColor: 'black'
-  });
-  var radial = new UI.Radial({
-    size: new Vector2(140, 140),
-    angle: 0,
-    angle2: 300,
-    radius: 20,
-    backgroundColor: 'cyan',
-    borderColor: 'celeste',
-    borderWidth: 1,
-  });
-  var textfield = new UI.Text({
-    size: new Vector2(140, 60),
-    font: 'gothic-24-bold',
-    text: 'Dynamic\nWindow',
-    textAlign: 'center'
-  });
-  var windSize = wind.size();
-  // Center the radial in the window
-  var radialPos = radial.position()
-      .addSelf(windSize)
-      .subSelf(radial.size())
-      .multiplyScalar(0.5);
-  radial.position(radialPos);
-  // Center the textfield in the window
-  var textfieldPos = textfield.position()
-      .addSelf(windSize)
-      .subSelf(textfield.size())
-      .multiplyScalar(0.5);
-  textfield.position(textfieldPos);
-  wind.add(radial);
-  wind.add(textfield);
-  wind.show();
-});
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+// register the traverson-hal plug-in for media type 'application/hal+json'
+traverson.registerMediaType(JsonHalAdapter.mediaType, JsonHalAdapter);
+
+// use Traverson to follow links, as usual
+var api = traverson.from('http://192.168.1.134:8080')
+        .jsonHal();
+var loadMenu = function (follow) {
+    api.newRequest()
+        .follow(follow.slice())
+        .getResource(function (error, resource) {
+            if (error) {
+                console.error('No luck :-)', error, follow);
+                return;
+            }
+
+            if (main) {
+                main.hide();
+                main = null;
+            }
+
+            console.log('We have followed the path and reached our destination.');
+            console.log(JSON.stringify(resource, null, '  '));
+            console.log(JSON.stringify(follow, null, '  '));
+
+            var items = _.map(resource, function (item) {
+                return {
+                    title: item.title
+                };
+            });
+
+            var menu = new UI.Menu({
+                sections: [{
+                        items: items
+                    }]
+            });
+            menu.on('select', function (e) {
+                var newFollow = follow.slice();
+                newFollow.unshift('item[' + e.itemIndex + ']');
+
+                if (resource[e.itemIndex].action) {
+                    var url = api.getFrom() + resource[e.itemIndex]._links.self.href;
+                    ajax({
+                        method: 'POST',
+                        url: url
+                    });
+                    return;
+                }
+                loadMenu(newFollow);
+            });
+            menu.show();
+        });
+};
+loadMenu(['item[$all]']);
